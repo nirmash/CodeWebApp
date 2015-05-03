97c7edbf7b1ca88ce5572e4f7fe010423a64c6c7
nirma <nirmsk@hotmail.com>
2015-05-03 00:35:34 -0700

    Commit 5

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Local change in Code</h1>');
+    res.end('<h1>Commit to Server</h1>');
 }).listen(process.env.PORT); 
